01a69185db8a9a7fa95f5ad7e504201213202d37
Anton Knapp <antonk3344@gmail.com>
2023-05-27 00:01:57 +0300

    Add UI animations

--- Assets/Core/UI/Gameplay/Scripts/LootCounter.cs
+++ Assets/Core/UI/Gameplay/Scripts/LootCounter.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
+using System;
 
 public class LootCounter : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _text;
-    [SerializeField] private int _itemsUIPoolSize;
+    [SerializeField] private float _animationDuration = 2f;
+    [SerializeField] private float _animationPulseIntensivity = 0.2f;
 
     public int Count { get; private set; }
 
@@ -18,6 +21,7 @@ public class LootCounter : MonoBehaviour
     public void Add()
     {
         Count++;
+        AnimateTextPulsed(0.05f, Count.ToString());
         _text.text = Count.ToString();
     }
 
@@ -25,5 +29,32 @@ public class LootCounter : MonoBehaviour
     {
         Count = 0;
         _text.text = Count.ToString();
+    }
+
+    public void AnimateToZero()
+    {
+        StartCoroutine(AnimateDiscarding());
+        Reset();
+    }
+
+    private IEnumerator AnimateDiscarding()
+    {
+        var duration = _animationDuration / Count;
+
+        for (int i = Count; i  >= 0; i--)
+        {
+            AnimateTextPulsed(duration, i.ToString());
+            yield return new WaitForSeconds(duration * 2);
+        }
+    }
+
+    private void AnimateTextPulsed(float duration, string content)
+    {
+        var currentScale = transform.localScale;
+
+        transform.DOScale(currentScale * (1 - _animationPulseIntensivity), duration)
+                .SetLoops(2, LoopType.Yoyo)
+                .SetLink(gameObject)
+                .OnKill(() => _text.text = content);
     }
 }
